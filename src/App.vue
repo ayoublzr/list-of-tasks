2c3ce8b378da7a77f669ee33a7bdfa8d327af06c
ayoublzr <ayoublzr93@gmail.com>
2024-09-09 19:37:30 +0200

    Fix rendering of MainView component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
-  
+  <section >
+    <h1>
+      My Tasks List
+    </h1>
+  </section>
+  <section>
+    <MainView />
+  </section>
 </template>
+
+<script setup>
+import MainView from './views/MainView.vue'
+</script>
 
 <style scoped>
 
---
+++ src/views/MainView.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+      <section>
+        <label>Filter tasks:</label>
+        <select>
+          <option value="all">All tasks</option>
+          <option value="completed">Completed tasks</option>
+          <option value="incomplete">Incomplete tasks</option>
+        </select>
+      </section>
+  
+    </main>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
