3d741088fcc8348be1e11e7f04ca4d8333c07d86
ayoublzr <ayoublzr93@gmail.com>
2024-09-09 20:31:30 +0200

    Add createTodo component

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,39 @@
+<template>
+    <h3>Create a Todo</h3>
+  
+    <form @submit.prevent="handleAddTodo">
+      <input
+        type="text"
+        placeholder="e.g. make a video"
+        :value="inputContent"
+        @input="updateInputContent($event.target.value)"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    inputContent: String, 
+  });
+  
+  
+  const emit = defineEmits(["addTodo", "update:inputContent"]);
+  
+  
+  const updateInputContent = (value) => {
+    emit("update:inputContent", value); 
+  };
+  
+  const handleAddTodo = () => {
+    if (props.inputContent.trim() !== "") {
+      emit("addTodo", props.inputContent);
+    }
+  };
+  </script>
+  
+  <style scoped>
+  
+  
+  </style>
+  
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,22 +1,55 @@
 <template>
-    <main>
-      <section>
-        <label>Filter tasks:</label>
-        <select>
-          <option value="all">All tasks</option>
-          <option value="completed">Completed tasks</option>
-          <option value="incomplete">Incomplete tasks</option>
-        </select>
-      </section>
+  <main>
+    <section class="create-todo">
+      <CreateTodo
+        :inputContent="inputContent"  
+        @update:inputContent="inputContent = $event"  
+        @addTodo="addTodo"  
+      />
+    </section>
+    <section>
+      <label>Filter tasks:</label>
+      <select>
+        <option value="all">All tasks</option>
+        <option value="completed">Completed tasks</option>
+        <option value="incomplete">Incomplete tasks</option>
+      </select>
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { ref, computed, watch, onMounted } from "vue";
+import CreateTodo from "../components/CreateTodo.vue";
+
+const todos = ref([]);
+const inputContent = ref("");
+
+
+
+const addTodo = (content) => {
+  todos.value.push({
+    content,  
+    done: false,  
+    createdAt: new Date().getTime(),  
+    isEditing: false,  
+  });
+  inputContent.value = "";  
+};
+onMounted(() => {
   
-    </main>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
+
+
+watch(
+  todos,
+  (newVal) => {
+   
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }  
+);
+</script>
+
+<style scoped></style>
